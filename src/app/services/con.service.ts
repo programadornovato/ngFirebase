d8f941229e9d6061883a78796b08d04176a6a3a9
Programador Novato <info@programadornovato.com>
2020-03-09 05:33:38 -0600

    06.- Proyecto con Firebase y Angular [Mostrar registros]🔥
    Video:
    https://youtu.be/vQbbQxniSMI

--- src/app/services/con.service.ts
+++ src/app/services/con.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-
+import { map } from 'rxjs/operators';
 export interface Item { name: string; }
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,13 @@ export class ConService {
   items: Observable<Item[]>;
   constructor(private afs: AngularFirestore) {
     this.itemsCollection = afs.collection<Item>('items');
-    this.items = this.itemsCollection.valueChanges();
+    this.items = this.itemsCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Item;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
   }
   retornaItems(){
     return this.items;
